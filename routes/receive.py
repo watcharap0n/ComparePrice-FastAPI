38d74d32720ba9a1a31b8cf990474f9767b5e331
watcharap0n <wera.watcharapon@gmail.com>
2021-09-20 13:47:05 +0700

    Update receive.py

--- routes/receive.py
+++ routes/receive.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from linebot.models import TextSendMessage
 from linebot import LineBotApi
 from features.flex_mesages import compare_price_flex
@@ -32,65 +32,67 @@ def get_profile(user_id, line_bot_api):
 )
 async def receive(item: ReceiveCard):
     """
-    Create a flex message to send LINE OA pass channel access token
+      Create a flex message to send LINE OA pass channel access token
 
-    -:key
-        - **line** : LIST or Array
-            **userId** : string
-            **channel_access_token** : string
-            **docno** : string
-            **docdate** : string
-            **duedate** : string
-            **remark** : string
-            **endpoint** : string
+      -:key
+          - **line** : LIST or Array
+              **userId** : string
+              **channel_access_token** : string
+              **docno** : string
+              **docdate** : string
+              **duedate** : string
+              **remark** : string
+              **endpoint** : string
 
-    description
-        - **line** : multiple object
-        - **userId** : token send user
-        - **channel_access_token** : LINE account
-        - **docno** : unknown
-        - **docdate** : unknown
-        - **duedate** : unknown
-        - **remark** : unknown
-        - **endpoint** : unknown
+      description
+          - **line** : multiple object
+          - **userId** : token send user
+          - **channel_access_token** : LINE account
+          - **docno** : unknown
+          - **docdate** : unknown
+          - **duedate** : unknown
+          - **remark** : unknown
+          - **endpoint** : unknown
 
-    :param item:
-    :return {
-            f'LINE OA: ': accounts,
-            'message': 'description',
-            'status': status (True, False)
-        }:
+      :param item:
+      :return {
+              f'LINE OA: ': accounts,
+              'message': 'description',
+              'status': status (True, False)
+          }:
     """
-    item = item.dict()
-    for i in item['line']:
-        line_bot_api = LineBotApi(i['channel_access_token'])
-        profile = get_profile(line_bot_api=line_bot_api, user_id=i['userId'])
-        i['displayName'] = profile['displayName']
-        i['img'] = profile['img']
-        i['status'] = profile['status']
-        _d = datetime.datetime.now()
-        i["date"] = _d.strftime("%d/%m/%y")
-        i["time"] = _d.strftime("%H:%M:%S")
-        db.insert_one(collection='send_flex', data=i)
-        del i['_id']
-        userId = i['userId']
-        print(i)
-        if i['remark']:
-            line_bot_api.push_message(userId,
-                                      compare_price_flex(mainname=i['mainname'], docdate=i['docdate'],
-                                                         acct_name=i['acct_name'], remark=i['remark'],
-                                                         duedate=i['duedate'], docno=i['docno'],
-                                                         endpoint=i['endpoint']))
-        else:
-            line_bot_api.push_message(userId,
-                                      compare_price_flex(mainname=i['mainname'], docdate=i['docdate'],
-                                                         acct_name=i['acct_name'], remark='ไม่มีข้อมูลเพิ่มเติม',
-                                                         duedate=i['duedate'], docno=i['docno'],
-                                                         endpoint=i['endpoint']))
-    return {
-        'message': 'please check your account LINE',
-        'status': True
-    }
+    try:
+        item = item.dict()
+        for i in item['line']:
+            line_bot_api = LineBotApi(i['channel_access_token'])
+            profile = get_profile(line_bot_api=line_bot_api, user_id=i['userId'])
+            i['displayName'] = profile['displayName']
+            i['img'] = profile['img']
+            i['status'] = profile['status']
+            _d = datetime.datetime.now()
+            i["date"] = _d.strftime("%d/%m/%y")
+            i["time"] = _d.strftime("%H:%M:%S")
+            db.insert_one(collection='send_flex', data=i)
+            del i['_id']
+            userId = i['userId']
+            if i['remark']:
+                line_bot_api.push_message(userId,
+                                          compare_price_flex(mainname=i['mainname'], docdate=i['docdate'],
+                                                             acct_name=i['acct_name'], remark=i['remark'],
+                                                             duedate=i['duedate'], docno=i['docno'],
+                                                             endpoint=i['endpoint']))
+            else:
+                line_bot_api.push_message(userId,
+                                          compare_price_flex(mainname=i['mainname'], docdate=i['docdate'],
+                                                             acct_name=i['acct_name'], remark='ไม่มีข้อมูลเพิ่มเติม',
+                                                             duedate=i['duedate'], docno=i['docno'],
+                                                             endpoint=i['endpoint']))
+        return {
+            'message': 'please check your account LINE',
+            'detail': {'status': True}
+        }
+    except:
+        raise HTTPException(status_code=400, detail={'status': False})
 
 
 @route.post(
@@ -131,22 +133,25 @@ async def receive(item: ReceiveText):
             'status': status (True, False)
         }:
     """
-    oa = []
-    item = item.dict()
-    for i in item['line']:
-        line_bot_api = LineBotApi(i['channel_access_token'])
-        profile = get_profile(line_bot_api=line_bot_api, user_id=i['userId'])
-        i['displayName'] = profile['displayName']
-        i['img'] = profile['img']
-        i['status'] = profile['status']
-        db.insert_one(collection='send_message', data=i)
-        userId = i['userId']
-        text = i['text']
-        line_bot_api.push_message(userId, TextSendMessage(text=text))
-        bot_info = line_bot_api.get_bot_info()
-        oa.append(bot_info.display_name)
-    return {
-        f'LINE OA: ': ", ".join(oa),
-        'message': 'please check your account LINE',
-        'status': True
-    }
+    try:
+        oa = []
+        item = item.dict()
+        for i in item['line']:
+            line_bot_api = LineBotApi(i['channel_access_token'])
+            profile = get_profile(line_bot_api=line_bot_api, user_id=i['userId'])
+            i['displayName'] = profile['displayName']
+            i['img'] = profile['img']
+            i['status'] = profile['status']
+            db.insert_one(collection='send_message', data=i)
+            userId = i['userId']
+            text = i['text']
+            line_bot_api.push_message(userId, TextSendMessage(text=text))
+            bot_info = line_bot_api.get_bot_info()
+            oa.append(bot_info.display_name)
+        return {
+            f'LINE OA: ': ", ".join(oa),
+            'message': 'please check your account LINE',
+            'detail': {'status': True}
+        }
+    except:
+        raise HTTPException(status_code=400, detail={'status': False})
